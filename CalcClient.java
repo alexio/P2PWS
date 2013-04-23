0f545c001fbad122cad7daaa5c805a9f52567d03
Hua Yang <huacyang@gmail.com>
2013-04-22 21:34:04 -0400

    replaced

--- CalcClient.java
+++ CalcClient.java
@@ -1,97 +1,71 @@
-// Alexio Mota
+/* @arthur: Hua Yang */
 import java.io.*;
 import java.net.*;
 
-public class CalcClient implements Runnable{
-
-	Socket conn;
-
-	CalcClient(Socket sock){
-		this.conn = sock;
-	}
-
-	public static void main(String args[]) throws Exception{
-
-		String server_name = "localhost";
+public class CalcClient {
+	public static void main(String args[]) throws Exception {
+		// default server name
+		String line, server = "localhost";
+		// default server port
 		int port = 12345;
-		if(args.length == 1){
-			server_name = args[0]; //if there is 1 arg, server is set to it according to specs.
-		}
-		else if(args.length == 2){
-			//if there are 2 args, both server and port are changed (as long as arg[1] is a valid input)
-			server_name = args[0];
-			try{
+		Socket sock = null;
+		BufferedReader userdata = new BufferedReader(new InputStreamReader(System.in));
+
+		if (args.length > 2) { // incorrect input
+			System.err.println("Usage: java TCPClient [server_name - optional [port - optional]]");
+			System.exit(1);
+		} else if (args.length == 1) { // sets server name to the one given by the user
+			server = args[0];
+		} else if (args.length == 2) { // sets server name and port to the ones given by the user
+			server = args[0];
+			try {
 				port = Integer.parseInt(args[1]);
-			}
-			catch(NumberFormatException nfe){
-				System.out.println(nfe);
+				if (port < 1024 || port > 65535) { // checks if input is out of bounds
+					System.out.println("Error: Port number out of bounds!");
+					System.exit(1);
+				}
+			} catch (NumberFormatException e) { // checks if given parameter is a number
+				System.out.println("Error: Port must be a number!");
 				System.exit(1);
 			}
-
-			if(port < 1024 || port > 65535){
-				System.out.println("Port number given does not fall within 1024 and 65535");
-				System.exit(2);
-			}
-			System.out.println("Server: " + server_name + " port " + port + "\n" );
-		}
-		else if(args.length > 0){
-			System.err.println("Incorrect number of inputs\n");
-			System.exit(3);
 		}
 
-
-		try{
-
-			Socket sock = null;
-			try{
-				sock = new Socket("localhost", 12345);
-			}
-			catch(UnknownHostException uhe){
-				System.out.println(uhe);
-				System.exit(4);
-			}
-
-			
-			if(sock == null){
-				System.out.println("Connection not made, exiting! \n");
-				System.exit(6);
-			}
-
-			//if connection is made, thread for user input is created. Thread also sends input to server
-			new Thread(new CalcClient(sock)).start();
-			
-			BufferedReader fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-			String msg;
-			while((msg = fromServer.readLine()) != null){ //while there's data from server
-				System.out.println("=> " + msg);
-			}
-			sock.close(); //close connection
+		// attempts to connect to the socket
+		try {
+		sock = new Socket(server, port);
+		System.out.println("Using port: " + port);
+		} catch (UnknownHostException e) { // error if the hostname is incorrect
+			System.out.println("Error: Hostname does not exist!");
+			System.exit(1);
+		} catch (ConnectException e) { // error if the server is full
+			System.out.println("Error: Server is currently full!");
+			System.exit(1);
+		} catch (NoRouteToHostException e) { // error if the port number is incorrect
+			System.out.println("Error: Port does not exist!");
+			System.exit(1);
+		} catch (IOException e) { // other socket errors
+			System.out.println("Error: " + e);
+			System.exit(1);
 		}
-		catch(IOException e){
-			System.out.println(e);
-			System.exit(7);
-		}
-	}
 
+		DataOutputStream toServer = new DataOutputStream(sock.getOutputStream());
+		BufferedReader fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		// delimiters to parse the output from server
+		String delimiters = "[;\\n]";
 
-	/*reads user input and send it to server*/
-	public void run(){
-		 
-		 try{
-
-			BufferedReader user_input = new BufferedReader(new InputStreamReader(System.in));
-		    DataOutputStream toServer = new DataOutputStream(conn.getOutputStream());
-
-		    String line;
-			while((line = user_input.readLine()) != null){
-				line.trim();
-				toServer.writeBytes(line + '\n');
+		// continues to read line from client till 'end' to close socket
+		while ((line = userdata.readLine()) != null) {
+			toServer.writeBytes(line + '\n');
+			// reads and parse output from server
+			String result = fromServer.readLine();
+			if (!result.equals("")) {
+				String[] tokens = result.split(delimiters);
+				for (int i = 0; i < tokens.length; i++) {
+					System.out.println(tokens[i]); 
+				}
 			}
+			if (line.equals("end")) { break; }
 		}
-		catch(IOException e){
-			System.out.println(e);
-			System.exit(5);
-		}
-		
+		sock.close();
 	}
 }
