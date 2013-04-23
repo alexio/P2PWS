cabd9fbba8a842408d324b00592048184a7196fa
Alexio <ignis@ignis-HP-ENVY-14-SPECTRE-Notebook-PC.(none)>
2013-04-22 21:50:27 -0400

    Add few changes to main

--- p2pws.java
+++ p2pws.java
@@ -13,8 +13,39 @@ public class p2pws implements Runnable{
 
 	public static void main(String[] args){
 
-		if(args.length > 1){
+		int port;
+		if(args.length != 1){
 			System.out.println("Invalid number of arguments\n");
+			return;
 		}
+
+		try{
+			port = Integer.parseInt(args[0]);
+		}
+		catch(NumberFormatException nfe){
+			System.out.println(nfe);
+			return;
+		}
+
+		try{
+			//Create server socket on provided port
+			ServerSocket server_sock = new ServerSocket(port);
+			for(;;){
+				Socket conn = server_sock.accept();
+				System.out.println("Connection Established");
+
+				//create thread for each new connection
+				new Thread(new p2pws(conn)).start();
+			}
+		}
+		catch(IOException e){
+			System.out.println(e);
+			return;
+		}
+
+	}
+
+	public void run(){
+
 	}
 }
