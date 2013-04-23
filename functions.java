7098dfb7d779cfebbbff3077be0d804bfe1cb00d
Hua Yang <huacyang@gmail.com>
2013-04-23 18:57:29 -0400

    placed all the inputs into one file

---
+++ functions.java
@@ -0,0 +1,31 @@
+/*
+ * @arthur: Hua Yang
+ * @arthur: Alexio Mota
+ */
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class functions {
+	
+    public String Delete(String url) {
+
+    }
+
+    public String Get(String url) {
+        if (url.equals("local.html") {
+            
+        }
+    }
+
+    public String MD5(String number) {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(s.getBytes(), 0, number.length());
+        System.out.println("MD5: " + md);
+    }
+
+    public String Put(String url) {
+
+    }
+
+}
