ff8211a4dd37f4b6f7ae66cc6baaef83e16b2a3f
Hua Yang <huacyang@gmail.com>
2013-04-23 23:07:35 -0400

    updated get

--- functions.java
+++ functions.java
@@ -5,27 +5,43 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.security.*;
+import java.math.*;
 
 public class functions {
 	
-    public String Delete(String url) {
-
+    /*
+     * Given the file url, calculate and return the MD5 hash value
+     */
+    public static int MD5(String url) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(url.getBytes(), 0, url.length());
+        return (new BigInteger(1, md.digest())).intValue();
     }
 
-    public String Get(String url) {
-        if (url.equals("local.html") {
+    public static String Delete(String url) {
+        return null;
+    }
+
+    /*
+     * Given the file url, provide HTTP reponses accordingly
+     */
+    public static String Get(String url) throws Exception {
+        // if file path is 'local.html', return with HTTP 200 OK response,
+        if (url.equals("local.html")) {
             
         }
+
+        int hashvalue = MD5(url);
+        System.out.println("MD5: " + hashvalue);
+        return null;
     }
 
-    public String MD5(String number) {
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        md.update(s.getBytes(), 0, number.length());
-        System.out.println("MD5: " + md);
+    public static String Put(String url) {
+        return null;
     }
 
-    public String Put(String url) {
-
+    public static void main(String args[]) throws Exception {
+        Get("local.html");
     }
-
 }
